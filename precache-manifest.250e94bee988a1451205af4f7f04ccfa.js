953c7c7deed5a63ad6576236ac04f14d8cc540c6
Jacob Lee <jacobtwlee@gmail.com>
2019-03-12 18:42:30 -0700

    Updates

---
+++ precache-manifest.250e94bee988a1451205af4f7f04ccfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1df889ef7e7a173dc6e6",
+    "url": "/connect-four-react/static/css/main.94342ab0.chunk.css"
+  },
+  {
+    "revision": "1df889ef7e7a173dc6e6",
+    "url": "/connect-four-react/static/js/main.1df889ef.chunk.js"
+  },
+  {
+    "revision": "1d4fca740620e001ae54",
+    "url": "/connect-four-react/static/js/runtime~main.1d4fca74.js"
+  },
+  {
+    "revision": "df1bc5dbe39de2d29435",
+    "url": "/connect-four-react/static/js/2.df1bc5db.chunk.js"
+  },
+  {
+    "revision": "87e770ee7a7163f6238296c3a4bb7748",
+    "url": "/connect-four-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/connect-four-react/precache-manifest.eb9c2b654e5e33e395d92f92cb8dd69f.js"
+  "/connect-four-react/precache-manifest.250e94bee988a1451205af4f7f04ccfa.js"
 );
 
 workbox.clientsClaim();
